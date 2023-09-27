981d1025b165167d4bfc6b4d3f6fa57f516747c9
radugaboost <skeal.smirnov2004@mail.ru>
2023-09-28 02:09:27 +0300

    hw1

---
+++ chart_parse.py
@@ -0,0 +1,21 @@
+import requests, pprint
+from bs4 import BeautifulSoup
+from config import (
+    URL, HEADERS, CHART_LINE, AUTHOR, TRACK, HTTP_OK
+)
+
+if __name__ == "__main__":
+    response = requests.get(URL, headers=HEADERS)
+
+    if response.status_code == HTTP_OK:
+        chart = dict()
+        soup = BeautifulSoup(response.text, "lxml")
+        lines = soup.find_all("div", class_=CHART_LINE)
+        for line in enumerate(lines):
+            author = line[1].find("a", class_=AUTHOR).text
+            track = line[1].find("a", class_=TRACK).text
+            chart[line[0] + 1] = {
+                author: " ".join(track.strip().split())
+            }
+
+        pprint.pprint(chart)
---
+++ config.py
@@ -0,0 +1,7 @@
+URL = "https://music.yandex.ru/chart"
+HEADERS = {"user-agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
+HTTP_OK = 200
+
+CHART_LINE = 'd-track typo-track d-track_selectable d-track_with-cover d-track_with-chart'
+AUTHOR = 'deco-link deco-link_muted'
+TRACK = 'd-track__title deco-link deco-link_stronger'
